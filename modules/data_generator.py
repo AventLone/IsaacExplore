dd0d93cd960c5b7c339d6c1a197570ec92790d33
Avent Lone <piggy.stoic@outlook.com>
2025-09-08 18:48:40 +0800

    :zap: Update

--- modules/data_generator.py
+++ modules/data_generator.py
@@ -6,15 +6,15 @@ import omni.usd
 from isaacsim.core.utils.stage import get_current_stage
 from pxr import Gf
 
-stage = get_current_stage()
-prim = stage.GetPrimAtPath("/World/AnyPrim")
 
 class ObjBasedDateGenerator:
     def __init__(self, config: dict) -> None:
         
         rep.orchestrator.set_capture_on_play(False)
         rep.set_global_seed(66)
-        # random
+        rep.randomizer.register(self._randomizeCameraPose)
+
+        self._random_trigger = rep.trigger.on_frame(max_execs=config["frames_required"], interval=1, rt_subframes=8)
 
         self._frames_required = config["frames_required"]
         self._obj_prim_path = config["obj_prim_path"]
@@ -27,46 +27,42 @@ class ObjBasedDateGenerator:
 
         # camera_prim_path = "/World/camera_data"
         # self._camera = rep.get.prim_at_path(camera_prim_path)
-        self._camera = rep.create.camera()
-        # if not self._camera:
-        #     raise RuntimeError(f"Camera not found: {camera_prim_path}")
-
-        # self.render_product = rep.create.render_product(camera="/World/camera_data/SG3S_ISX031C_GMSL2F_H190XA_01",
-        #                                                 resolution=(1024, 1024))
-        # self._render_product = rep.create.render_product(camera="/OmniverseKit_Persp", resolution=(1024, 1024))
+        # self._camera = rep.create.camera()
+        self._camera = rep.create.camera(
+            focus_distance=400.0, focal_length=15.0, clipping_range=(0.1, 10000000.0), name="DriverCam"
+        )
         self._render_product = rep.create.render_product(camera=self._camera, resolution=(1024, 1024))
 
         self._writer = rep.writers.get("BasicWriter")
         data_save_dir = os.path.join(os.getcwd(), "data/semantic_segmentation")
         self._writer.initialize(output_dir=data_save_dir, rgb=True, semantic_segmentation=True)
-        self._writer.attach(self._render_product)
+        self._writer.attach(self._render_product, trigger=self._random_trigger)
 
         self.is_finished = False
 
-    # def __objPosition(self):
-    # @property
-    # def obj_postion(self):
-    #     return omni.usd.get_world_transform_matrix(prim).ExtractTranslation()
+        self._camera_position_domain = [self._obj_position - self._camera_position_domain_lower,
+                                         self._obj_position + self._camera_position_domain_upper]
 
+    def __setObjPosition(self):
+        pass
 
-    def __randomizeCameraPose(self):
+    def _randomizeCameraPose(self) -> rep.scripts.utils.ReplicatorItem:
         with self._camera:
             rep.modify.pose(
-                # position=rep.distribution.uniform((-5, 5, 1), (-1, 15, 3)),
                 position=rep.distribution.uniform(
                     self._obj_position - self._camera_position_domain_lower, 
                     self._obj_position + self._camera_position_domain_upper
                 ),
                 look_at=self._obj_prim_path
             )
+        return self._camera.node
 
     def generate(self):
-        for _ in range(self._frames_required):
-            self.__randomizeCameraPose()
-            rep.orchestrator.step()
+        with self._random_trigger:
+            rep.randomizer._randomizeCameraPose()   # ← 随机器调用“必须”放在 trigger 里
 
+        rep.orchestrator.run()
+        rep.orchestrator.wait_until_complete()
         self._writer.detach()
         self._render_product.destroy()
-        rep.orchestrator.wait_until_complete()
-
         self.is_finished = True
